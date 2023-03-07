2226e7228e56b23837d53f6127451bea8062134f
Zelgadis <chrisgelion@gmail.com>
2023-03-07 15:17:06 -0300

    Aplicado el search y filtros en index.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,8 @@ import data from "./data.js";
 const categorias = [
   ...new Set(data.events.map((item) => item.category)),
 ].sort();
-console.log(categorias);
-
-/* const categorias = {};
-let arrays = [];
-arrays = arrCategorias.filter((item) =>
-  categorias[item.category] ? false : (categorias[item.category] = true)
-); */
+let categoChk = [];
+const input = document.querySelector('input[type="search"]');
 
 const fragmentC = document.createDocumentFragment();
 const containerC = document.getElementsByClassName("categorias");
@@ -35,18 +30,20 @@ const fragment = document.createDocumentFragment();
 const container = document.getElementsByClassName("row");
 
 function creoCards(data, contenedor) {
-  for (let i = 0; i < data.events.length; i++) {
+  contenedor[0].innerHTML = "";
+
+  for (let i = 0; i < data.length; i++) {
     let div = document.createElement("div");
     div.className =
       "col-lg-3 col-md-4 col-sm-6 mb-3 d-flex align-items-stretch";
     div.innerHTML += `
         <div class="card">
-            <img src="${data.events[i].image}" class="card-img-top" alt="Card Image">
+            <img src="${data[i].image}" class="card-img-top" alt="Card Image">
             <div class="card-body d-flex flex-column">
-                <h5 class="card-title">${data.events[i].name}</h5>
-                <p class="card-text">${data.events[i].description}</p>
+                <h5 class="card-title">${data[i].name}</h5>
+                <p class="card-text">${data[i].description}</p>
                 <div class="card-footer d-flex flex-row justify-content-between align-items-center">
-                    <p>Price $${data.events[i].price}</p>
+                    <p>Price $${data[i].price}</p>
                     <a href="#" class="btn btn-primary mt-auto align-self-start see-more">See more...</a>
                 </div>
             </div>
@@ -57,5 +54,33 @@ function creoCards(data, contenedor) {
   contenedor[0].appendChild(fragment);
 }
 
+function filtroData() {
+  let arrays = data.events;
+  if (categoChk.length > 0)
+    arrays = data.events.filter((item) => categoChk.includes(item.category));
+
+  if (input.value.length > 0)
+    arrays = arrays.filter((item) =>
+      item.name.toLocaleLowerCase().includes(input.value.toLocaleLowerCase())
+    );
+
+  creoCards(arrays, container);
+}
+
+let checkboxes = document.querySelectorAll("input[type=checkbox]");
+
+checkboxes.forEach(function (checkbox) {
+  checkbox.addEventListener("change", function () {
+    categoChk = Array.from(checkboxes)
+      .filter((i) => i.checked)
+      .map((i) => i.value);
+    filtroData();
+  });
+});
+
+input.addEventListener("keyup", () => {
+  filtroData();
+});
+
 creoCategorias(categorias, containerC);
-creoCards(data, container);
+creoCards(data.events, container);
