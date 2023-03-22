d1149ea8fee2bbcd5fedaa338e2401888b88f12b
Zelgadis <chrisgelion@gmail.com>
2023-03-22 02:40:27 -0300

    Modificacion stats vue

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ createApp({
   data() {
     return {
       vueDatos: [],
-      vueDatosFilter: undefined,
+      vueDatosFilter: [],
       vueCategorias: [],
       textoBus: "",
       checked: [],
--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,172 +1,174 @@
-let categoChk = [];
-let categorias = [];
-let data = { currentDate: "", events: [] };
-const input = document.querySelector('input[type="search"]');
+const { createApp } = Vue;
 
-const fragmentE = document.createDocumentFragment();
-const containerE = document.getElementsByClassName("event");
+createApp({
+  data() {
+    return {
+      vueDatos: [],
+      vueEvent: [],
+      vueUpcoming: [],
+      vuePast: [],
+    };
+  },
+  created() {
+    fetch("./assets/amazing.json")
+      .then((res) => res.json())
+      .then((res) => {
+        this.vueDatos = res.events;
 
-function creoEvent(data, contenedor) {
-  let tr = document.createElement("tr");
-  let eventMayor = data.events.sort((a, b) => {
-    return (
-      Number.parseFloat(
-        b.assistance ? b.assistance / b.capacity : b.estimate / b.capacity
-      ) -
-      Number.parseFloat(
-        a.assistance ? a.assistance / a.capacity : a.estimate / a.capacity
-      )
-    );
-  })[0].name;
+        this.vueEvent = this.creoEvent(res.events);
+        this.vueUpcoming = this.creoUpcoming(res);
+        this.vuePast = this.creoPast(res);
+      });
+  },
+  methods: {
+    creoEvent(datos) {
+      let eventMayor = datos.sort((a, b) => {
+        return (
+          Number.parseFloat(
+            b.assistance ? b.assistance / b.capacity : b.estimate / b.capacity
+          ) -
+          Number.parseFloat(
+            a.assistance ? a.assistance / a.capacity : a.estimate / a.capacity
+          )
+        );
+      })[0].name;
 
-  let eventMenor = data.events.sort((a, b) => {
-    return (
-      Number.parseFloat(
-        a.assistance ? a.assistance / a.capacity : a.estimate / a.capacity
-      ) -
-      Number.parseFloat(
-        b.assistance ? b.assistance / b.capacity : b.estimate / b.capacity
-      )
-    );
-  })[0].name;
+      let eventMenor = datos.sort((a, b) => {
+        return (
+          Number.parseFloat(
+            a.assistance ? a.assistance / a.capacity : a.estimate / a.capacity
+          ) -
+          Number.parseFloat(
+            b.assistance ? b.assistance / b.capacity : b.estimate / b.capacity
+          )
+        );
+      })[0].name;
 
-  let eventCapacity = data.events.sort((a, b) => {
-    return Number.parseFloat(b.capacity) - Number.parseFloat(a.capacity);
-  })[0].name;
+      let eventCapacity = datos.sort((a, b) => {
+        return Number.parseFloat(b.capacity) - Number.parseFloat(a.capacity);
+      })[0].name;
 
-  tr.innerHTML += `
-        <td>${eventMayor}</td>
-        <td>${eventMenor}</td>
-        <td>${eventCapacity}</td>
-    `;
-  fragmentE.appendChild(tr);
-  contenedor[0].appendChild(fragmentE);
-}
+      const arrayA = {
+        mayor: eventMayor,
+        menor: eventMenor,
+        capacity: eventCapacity,
+      };
 
-const fragmentU = document.createDocumentFragment();
-const containerU = document.getElementsByClassName("upcoming");
+      return arrayA;
+    },
+    creoUpcoming(datos) {
+      let filtrado = [];
+      let result = [];
 
-function creoUpcoming(datos, contenedor) {
-  let filtrado = [];
+      for (let i = 0; i < datos.events.length; i++) {
+        if (datos.currentDate > datos.events[i].date) continue;
 
-  for (let i = 0; i < datos.events.length; i++) {
-    if (datos.currentDate > datos.events[i].date) continue;
+        filtrado.push(datos.events[i]);
+      }
 
-    filtrado.push(datos.events[i]);
-  }
+      let categorias = [
+        ...new Set(filtrado.map((item) => item.category)),
+      ].sort();
 
-  categorias = [...new Set(filtrado.map((item) => item.category))].sort();
+      for (let i = 0; i < categorias.length; i++) {
+        let capacity = filtrado
+          .filter((item) => item.category === categorias[i])
+          .reduce(
+            (sum, value) =>
+              typeof value.capacity == "number" ? sum + value.capacity : sum,
+            0
+          );
 
-  contenedor[0].innerHTML = "";
+        let estimate =
+          (filtrado
+            .filter((item) => item.category === categorias[i])
+            .reduce(
+              (sum, value) =>
+                typeof value.estimate == "number" ? sum + value.estimate : sum,
+              0
+            ) *
+            100) /
+          capacity;
 
-  for (let i = 0; i < categorias.length; i++) {
-    let capacity = filtrado
-      .filter((item) => item.category === categorias[i])
-      .reduce(
-        (sum, value) =>
-          typeof value.capacity == "number" ? sum + value.capacity : sum,
-        0
-      );
+        let ingresos = filtrado
+          .filter((item) => item.category === categorias[i])
+          .reduce(
+            (sum, value) =>
+              typeof value.price == "number"
+                ? sum + value.estimate * value.price
+                : sum,
+            0
+          )
+          .toLocaleString();
 
-    let estimate =
-      (filtrado
-        .filter((item) => item.category === categorias[i])
-        .reduce(
-          (sum, value) =>
-            typeof value.estimate == "number" ? sum + value.estimate : sum,
-          0
-        ) *
-        100) /
-      capacity;
+        const arrayA = {
+          categoria: categorias[i],
+          ingresos: ingresos,
+          estimado: estimate.toFixed(2),
+        };
 
-    let ingresos = filtrado
-      .filter((item) => item.category === categorias[i])
-      .reduce(
-        (sum, value) =>
-          typeof value.price == "number"
-            ? sum + value.estimate * value.price
-            : sum,
-        0
-      )
-      .toLocaleString();
+        result.push(arrayA);
+      }
 
-    let tr = document.createElement("tr");
-    tr.innerHTML += `
-        <td>${categorias[i]}</td>
-        <td>$ ${ingresos}</td>
-        <td>${estimate.toFixed(2)} %</td>
-    `;
-    fragmentU.appendChild(tr);
-  }
-  contenedor[0].appendChild(fragmentU);
-}
+      return result;
+    },
+    creoPast(datos) {
+      let filtrado = [];
+      let result = [];
 
-const fragmentP = document.createDocumentFragment();
-const containerP = document.getElementsByClassName("past");
+      for (let i = 0; i < datos.events.length; i++) {
+        if (datos.currentDate <= datos.events[i].date) continue;
 
-function creoPast(datos, contenedor) {
-  let filtrado = [];
+        filtrado.push(datos.events[i]);
+      }
 
-  for (let i = 0; i < datos.events.length; i++) {
-    if (datos.currentDate <= datos.events[i].date) continue;
+      let categorias = [
+        ...new Set(filtrado.map((item) => item.category)),
+      ].sort();
 
-    filtrado.push(datos.events[i]);
-  }
+      for (let i = 0; i < categorias.length; i++) {
+        let capacity = filtrado
+          .filter((item) => item.category === categorias[i])
+          .reduce(
+            (sum, value) =>
+              typeof value.capacity == "number" ? sum + value.capacity : sum,
+            0
+          );
 
-  categorias = [...new Set(filtrado.map((item) => item.category))].sort();
+        let assistance =
+          (filtrado
+            .filter((item) => item.category === categorias[i])
+            .reduce(
+              (sum, value) =>
+                typeof value.assistance == "number"
+                  ? sum + value.assistance
+                  : sum,
+              0
+            ) *
+            100) /
+          capacity;
 
-  contenedor[0].innerHTML = "";
+        let ingresos = filtrado
+          .filter((item) => item.category === categorias[i])
+          .reduce(
+            (sum, value) =>
+              typeof value.price == "number"
+                ? sum + value.assistance * value.price
+                : sum,
+            0
+          )
+          .toLocaleString();
 
-  for (let i = 0; i < categorias.length; i++) {
-    let capacity = filtrado
-      .filter((item) => item.category === categorias[i])
-      .reduce(
-        (sum, value) =>
-          typeof value.capacity == "number" ? sum + value.capacity : sum,
-        0
-      );
+        const arrayA = {
+          categoria: categorias[i],
+          ingresos: ingresos,
+          assistance: assistance.toFixed(2),
+        };
 
-    let assistance =
-      (filtrado
-        .filter((item) => item.category === categorias[i])
-        .reduce(
-          (sum, value) =>
-            typeof value.assistance == "number" ? sum + value.assistance : sum,
-          0
-        ) *
-        100) /
-      capacity;
+        result.push(arrayA);
+      }
 
-    let ingresos = filtrado
-      .filter((item) => item.category === categorias[i])
-      .reduce(
-        (sum, value) =>
-          typeof value.price == "number"
-            ? sum + value.assistance * value.price
-            : sum,
-        0
-      )
-      .toLocaleString();
-
-    let tr = document.createElement("tr");
-    tr.innerHTML += `
-        <td>${categorias[i]}</td>
-        <td>$ ${ingresos}</td>
-        <td>${assistance.toFixed(2)} %</td>
-    `;
-    fragmentP.appendChild(tr);
-  }
-  contenedor[0].appendChild(fragmentP);
-}
-
-async function getData() {
-  await fetch("./assets/amazing.json")
-    .then((res) => res.json())
-    .then((res) => (data = res));
-
-  creoEvent(data, containerE);
-  creoUpcoming(data, containerU);
-  creoPast(data, containerP);
-}
-
-getData();
+      return result;
+    },
+  },
+}).mount("#vue");
